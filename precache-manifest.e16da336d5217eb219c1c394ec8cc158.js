aaa5e474f2cc7673129f0511a980eee3fa36b6e1
Vanderson Oliveira da Silva <vanderson.th@gmail.com>
2023-01-11 22:33:11 -0300

    initial

---
+++ precache-manifest.e16da336d5217eb219c1c394ec8cc158.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70636ffc66228318fa3a1111c3090ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b489d7a320b774385a6c",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "8186b2bf9da7a453ebf4",
+    "url": "/static/css/main.bf447c50.chunk.css"
+  },
+  {
+    "revision": "b489d7a320b774385a6c",
+    "url": "/static/js/2.9b6149d0.chunk.js"
+  },
+  {
+    "revision": "8186b2bf9da7a453ebf4",
+    "url": "/static/js/main.d3daed90.chunk.js"
+  },
+  {
+    "revision": "19d590042be05d94698e",
+    "url": "/static/js/runtime-main.9bf3308e.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/static/media/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.0509ab09.woff2"
+  }
+]);
